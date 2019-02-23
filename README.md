5f80099e80639fd54b75786926ccf9a560b7cd1b
rajathavalam <raja.thavalam@senecaglobal.com>
2019-02-23 23:48:43 +0530

    Intial Commit

---
+++ README.md
@@ -0,0 +1,65 @@
+
+# react-material-ui-application
+
+Simple POC  of React.js nad Material UI
+
+#  Requirements
+
+The angular project needs to be a minimum version of listed below.
+
+1. React 16.x 
+2. Node.js 10.x and npm 6.x
+
+#React Project setup
+
+1. git clone https://github.com/rajaramtt/react-material-ui-application.git
+2. cd react-material-ui-application
+3. 
+```sh
+npm install
+```
+4. 
+```sh
+npm run start
+```
+
+5. Open brower `http://localhost:3000/`
+
+
+6. unit tests  - Pending- Skip now
+`npm test`
+
+
+7. Build 
+`npm build`
+
+## Help/Assistance
+
+Developer: Raja Rama Mohan Thavalam <rajaram.tavalam@gmail.com>  
+
+
+## License
+
+
+(The MIT License)
+
+Copyright (c) 2018 Raja Rama Mohan T <rajaram.tavalam@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/pages/index.js
@@ -0,0 +1,98 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import withRoot from '../withRoot';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+
+
+});
+
+
+class TextFields extends React.Component {
+  state = {
+    name: 'Cat in the Hat',
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+
+      <div>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+             
+              <Typography variant="h6" color="inherit" className={classes.grow}>
+              Web Extractor
+          </Typography>
+              
+            </Toolbar>
+          </AppBar>
+        </div>
+
+
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            id="standard-name"
+            label="Name"
+            className={classes.textField}
+            value={this.state.name}
+            onChange={this.handleChange('name')}
+            margin="normal"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+TextFields.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+// export default withStyles(styles)(TextFields);
+
+export default withRoot(withStyles(styles)(TextFields));
